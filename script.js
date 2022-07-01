c0522a2834010bb1334b8f57a21ea1f9fca874b9
Faisal Fajar Robani <94780882+faisalfajarrobani@users.noreply.github.com>
2022-07-01 15:59:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-window.addEventListener('scroll', reveal);
-function reveal() {
-  var reveals = document.querySelectorAll('.reveal');
+window.addEventListener('scroll', fadeefek);
+function fadeefek() {
+  var reveals = document.querySelectorAll('.fadeefek');
   for (var i = 0; i < reveals.length; i++) {
     var windowheight = window.innerHeight;
     var revealtop = reveals[i].getBoundingClientRect().top;
     var revealpoint = 200;
 
     if (revealtop > windowheight - revealpoint) {
-      reveals[i].classList.add('active');
+      reveals[i].classList.add('aktif');
     } else {
-      reveals[i].classList.remove('active');
+      reveals[i].classList.remove('aktif');
     }
   }
 }
