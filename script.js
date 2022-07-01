399e90ca6f298465df18a1d746b9421d52a55092
Faisal Fajar Robani <94780882+faisalfajarrobani@users.noreply.github.com>
2022-07-01 15:54:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function reveal() {
   for (var i = 0; i < reveals.length; i++) {
     var windowheight = window.innerHeight;
     var revealtop = reveals[i].getBoundingClientRect().top;
-    var revealpoint = 150;
+    var revealpoint = 200;
 
-    if (revealtop < windowheight - revealpoint) {
+    if (revealtop > windowheight - revealpoint) {
       reveals[i].classList.add('active');
     } else {
       reveals[i].classList.remove('active');
